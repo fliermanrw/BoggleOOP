2663b9cbd14bf285b3a55298f8177a7361d49391
Ryan Flierman <r.w.flierman@st.hanze.nl>
2017-04-21 16:01:17 +0200

    Find neighbours

--- src/Controller/BoggleController.java
+++ src/Controller/BoggleController.java
@@ -7,10 +7,13 @@ import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Random;
 
 
 public class BoggleController {
+    private final int fieldSize = 4;
     public GridPane boggleboardfield;
     public Pane wordfield;
 
@@ -23,15 +26,15 @@ public class BoggleController {
 
 
     private void generateNewBoard(){
-        for (int row = 0; row < 4; row++) {
+        for (int row = 0; row < fieldSize; row++) {
             boggleboardfield.addRow(row);
-            for (int col = 0; col < 4; col++) {
+            for (int col = 0; col < fieldSize; col++) {
                 Button a = new Button();
-                a.setMinWidth(100.0);
-                a.setMinHeight(100.0);
+                a.setMinWidth(70.0);
+                a.setMinHeight(70.0);
                 a.setText(String.valueOf(assignLetter()));
 
-                a.setId(String.valueOf(row * 4 + col));
+                a.setId(String.valueOf(row * fieldSize + col));
                 boggleboardfield.addColumn(col, a);
             }
 
@@ -68,9 +71,222 @@ public class BoggleController {
             Button button = (Button) node;
             System.out.println(button.getText());
         } else{
-            System.out.println("niks kunnen vinden x");
+            System.out.println("Niks kunnen vinden x");
+        }
+        System.out.println("xxxxxx");
+        System.out.println(findNeighbours(6));
+        System.out.println(findNeighbours(1));
+
+    }
+
+    public ArrayList<Integer> findNeighbours(int id) {
+        int temp;
+        HashSet noDups = new HashSet();
+
+        /*for (int cnt = 0; cnt < boggleboardfield.getChildren().size(); cnt++) {
+            int x = Integer.parseInt(boggleboardfield.getChildren().get(cnt).getId());
+            System.out.println(x);
+        }*/
+
+        // Linkerkant checks
+        if((id % fieldSize == 0)){
+
+            if(id < fieldSize-1) {
+                temp = id + 1;
+                noDups.add(temp);
+                System.out.println("Added: " + temp);
+
+                temp = id + fieldSize;
+                noDups.add(temp);
+                System.out.println("Added: " + temp);
+
+                temp = id + fieldSize + 1;
+                noDups.add(temp);
+                System.out.println("Added: " + temp);
+            }
+
+            if(id > (boggleboardfield.getChildren().size() -fieldSize)) {
+                temp = id - 1;
+                noDups.add(temp);
+                System.out.println("added: " + temp);
+
+                temp = id - fieldSize;
+                noDups.add(temp);
+                System.out.println("Added: " + temp);
+
+                temp = id - (fieldSize + 1);
+                noDups.add(temp);
+                System.out.println("Added: " + temp);
+            }
+
+            if(!(id < fieldSize-1) && !(id > (boggleboardfield.getChildren().size() -fieldSize))){
+                temp = id - fieldSize;
+                noDups.add(temp);
+                System.out.println("Added: " + temp);
+
+                temp = id - (fieldSize + 1);
+                noDups.add(temp);
+                System.out.println("Added: " + temp);
+
+                temp = id + 1;
+                noDups.add(temp);
+                System.out.println("added: " + temp);
+
+                temp = id + (fieldSize -1);
+                noDups.add(temp);
+                System.out.println("Added: " + temp);
+
+                temp = id - fieldSize;
+                noDups.add(temp);
+                System.out.println("Added: " + temp);
+
+
+            }
         }
 
+        //Rechterzijkant Check
+        if((id % fieldSize == fieldSize-1)){
+
+            if(id < fieldSize-1) {
+                temp = id - 1;
+                noDups.add(temp);
+                System.out.println("Added: " + temp);
+
+                temp = id + fieldSize;
+                noDups.add(temp);
+                System.out.println("Added: " + temp);
+
+                temp = id + fieldSize - 1;
+                noDups.add(temp);
+                System.out.println("Added: " + temp);
+            }
+
+            if(id > (boggleboardfield.getChildren().size() -fieldSize)) {
+                temp = id - 1;
+                noDups.add(temp);
+                System.out.println("added: " + temp);
+
+                temp = id - fieldSize;
+                noDups.add(temp);
+                System.out.println("Added: " + temp);
+
+                temp = id - (fieldSize -1);
+                noDups.add(temp);
+                System.out.println("Added: " + temp);
+            }
+
+            if(!(id < fieldSize-1) && !(id > (boggleboardfield.getChildren().size() - fieldSize))){
+                temp = id - (fieldSize + 1);
+                noDups.add(temp);
+                System.out.println("Added: " + temp);
+
+                temp = id - fieldSize;
+                noDups.add(temp);
+                System.out.println("Added: " + temp);
+
+                temp = id - 1;
+                noDups.add(temp);
+                System.out.println("added: " + temp);
+
+                temp = id + (fieldSize);
+                noDups.add(temp);
+                System.out.println("Added: " + temp);
+
+                temp = id - (fieldSize + 1);
+                noDups.add(temp);
+                System.out.println("Added: " + temp);
+
+            }
+        }
+
+        // Binnengetallen...
+        if((id > fieldSize-1) && (id < (boggleboardfield.getChildren().size() - fieldSize)) && ((id % fieldSize != 0)) && ((id % fieldSize != fieldSize -1))){
+            temp = id - (fieldSize + 1);
+            noDups.add(temp);
+            System.out.println("added: " + temp);
+
+            temp = id - (fieldSize);
+            noDups.add(temp);
+            System.out.println("added: " + temp);
+
+            temp = id - (fieldSize -1);
+            noDups.add(temp);
+            System.out.println("added: " + temp);
+
+            temp = id - 1;
+            noDups.add(temp);
+            System.out.println("added: " + temp);
+
+            temp = id + 1;
+            noDups.add(temp);
+            System.out.println("added: " + temp);
+
+            temp = id + (fieldSize - 1);
+            noDups.add(temp);
+            System.out.println("added: " + temp);
+
+            temp = id + (fieldSize);
+            noDups.add(temp);
+            System.out.println("added: " + temp);
+
+            temp = id + (fieldSize + 1);
+            noDups.add(temp);
+            System.out.println("added: " + temp);
+        }
+
+        // Bovengetallen
+        if((id < fieldSize) && (id % fieldSize != 0) && id % fieldSize != fieldSize-1){
+            temp = id - 1;
+            noDups.add(temp);
+            System.out.println("added: " + temp);
+
+            temp = id + 1;
+            noDups.add(temp);
+            System.out.println("added: " + temp);
+
+            temp = id + (fieldSize - 1);
+            noDups.add(temp);
+            System.out.println("added: " + temp);
+
+            temp = id + (fieldSize);
+            noDups.add(temp);
+            System.out.println("added: " + temp);
+
+            temp = id + (fieldSize + 1);
+            noDups.add(temp);
+            System.out.println("added: " + temp);
+        }
+
+        // Ondergetallen
+        if((id > (boggleboardfield.getChildren().size() - fieldSize)) && (id % fieldSize != 0) && id % fieldSize != fieldSize-1){
+            temp = id - (fieldSize + 1);
+            noDups.add(temp);
+            System.out.println("added: " + temp);
+
+            temp = id - (fieldSize);
+            noDups.add(temp);
+            System.out.println("added: " + temp);
+
+            temp = id - (fieldSize - 1);
+            noDups.add(temp);
+            System.out.println("added: " + temp);
+
+            temp = id - 1;
+            noDups.add(temp);
+            System.out.println("added: " + temp);
+
+            temp = id + 1;
+            noDups.add(temp);
+            System.out.println("added: " + temp);
+
+
+        }
+
+
+
+        System.out.println("xxxxxxxxxxxxxxxxxxxxx");
+
+        return new ArrayList<Integer>(noDups);
     }
 }
 
